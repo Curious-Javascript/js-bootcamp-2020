759295902313367b80b65aeda16405db423867c3
Zahar Gulchak <gulchak.zahar@gmail.com>
2020-06-01 12:21:24 +0300

    Fixed lint issue.

--- submissions/Sania/lesson11/js/index.js
+++ submissions/Sania/lesson11/js/index.js
@@ -1,22 +1,28 @@
+/* eslint-disable no-unused-vars */
 const combineArray = (arr) => {
-    document.getElementsByClassName('combine-array--answer')[0].innerHTML = arr.reduce((newArray, current) => newArray.concat(current), []);
+  document.getElementsByClassName(
+    'combine-array--answer',
+  )[0].innerHTML = arr.reduce(
+    (newArray, current) => newArray.concat(current),
+    [],
+  );
 };
 
-function every (array, condition) {
-    const answer = document.getElementsByClassName('every--answer')[0];
-    for (const item of array) {
-        if (!condition(item)) {
-            answer.innerHTML = 'false';
-        }
+function every(array, condition) {
+  const answer = document.getElementsByClassName('every--answer')[0];
+  for (const item of array) {
+    if (!condition(item)) {
+      answer.innerHTML = 'false';
     }
-    answer.innerHTML = 'true';
+  }
+  answer.innerHTML = 'true';
 }
-function some (array, condition) {
-    const answer = document.getElementsByClassName('some--answer')[0];
-    for (const item of array) {
-        if (condition(item)) {
-            answer.innerHTML = 'true';
-        }
+function some(array, condition) {
+  const answer = document.getElementsByClassName('some--answer')[0];
+  for (const item of array) {
+    if (condition(item)) {
+      answer.innerHTML = 'true';
     }
-    answer.innerHTML = 'false';
+  }
+  answer.innerHTML = 'false';
 }
